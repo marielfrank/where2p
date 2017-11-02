0f28492be034ab000b82f15da0bd65d4a3a531d6
marielfrank <marielbeth@gmail.com>
2017-11-02 17:45:34 -0400

    Add validations, associations, & methods to restroom.

--- app/models/restroom.rb
+++ app/models/restroom.rb
@@ -1,2 +1,23 @@
 class Restroom < ApplicationRecord
+    validates :location_id, presence: true
+    validates :name, uniqueness: true
+
+    belongs_to :location
+    delegate :neighborhood, :to => :location
+
+    has_many :ratings
+    has_many :users, :through => :ratings
+
+    has_many :restaurant_tags
+    has_many :tags, :through => :restaurant_tags
+
+    def average_rating
+        ratings.inject do |sum, rating|
+            sum + rating.value
+        end/(ratings.size)
+    end
+
+    def address
+        location.address
+    end
 end
