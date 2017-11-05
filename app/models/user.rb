bb2ef24986cb9866ca88b9973e41c84abc842f46
marielfrank <marielbeth@gmail.com>
2017-11-04 22:59:51 -0400

    Add user, associations, validations, bcrypt, dotenv.

---
+++ app/models/user.rb
@@ -0,0 +1,8 @@
+class User < ApplicationRecord
+    validates :name, presence: true
+    validates :email, presence: true, uniqueness: true
+    has_secure_password
+    
+    has_many :ratings
+    has_many :restrooms, :through => :ratings
+end
