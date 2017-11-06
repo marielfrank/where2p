e5e70eaa649dfe735058d54ad32d79e2b23f671a
marielfrank <marielbeth@gmail.com>
2017-11-06 18:19:04 -0500

    Switch rating to nested restroom resource. Icon edits.

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,10 @@ Rails.application.routes.draw do
   get '/signup' => 'users#new'
   get '/login' => 'sessions#new'
   post '/logout' => 'sessions#destroy'
-  resources :restrooms, :locations, :neighborhoods, :ratings, :users
+  resources :restrooms do
+    resources :ratings
+  end
+  resources :locations, :neighborhoods, :users
   resources :tags, only: [:new, :create, :show]
   resources :sessions, only: [:new, :create, :destroy]
 end
