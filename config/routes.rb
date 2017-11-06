5e75b81eaf375e3073b0485577edbb8d841117c1
marielfrank <marielbeth@gmail.com>
2017-11-06 12:48:05 -0500

    Migrate database, add RESTful routes.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: 'welcome#home'
+  get '/signup' => 'user#new'
+  get '/login' => 'session#new'
+  post '/logout' => 'session#destroy'
+  resources :restroom, :location, :neighborhood, :tag, :rating, :user
+  resources :session, only: [:new, :create, :destroy]
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171102223725) do
+ActiveRecord::Schema.define(version: 20171105025239) do
 
   create_table "locations", force: :cascade do |t|
     t.string "address"
@@ -49,6 +49,14 @@ ActiveRecord::Schema.define(version: 20171102223725) do
 
   create_table "tags", force: :cascade do |t|
     t.string "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
