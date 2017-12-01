fedc3f5080fec354c18af9b5918226abfe0f2d13
marielfrank <marielbeth@gmail.com>
2017-12-01 16:40:20 -0500

    Refactor, clean up.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+    def no_resource(resource)
+        if logged_in?
+            "Be the first to add a #{resource} this restroom!"
+        else
+            "No one has added #{resource}s to this restroom yet. Sign in to add one!"
+        end
+    end
 end
