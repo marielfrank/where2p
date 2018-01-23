56b9a8d5a14c349fe4edc9e1b7ded33367f9dbcf
marielfrank <marielbeth@gmail.com>
2018-01-23 15:38:47 -0500

    Fix handlebars partial on restrooms/show, add list ratings functionality in ratings.js

--- app/assets/javascripts/ratings.js
+++ app/assets/javascripts/ratings.js
@@ -1,23 +1,33 @@
 $(function() {
+    Rating.template = $('#rating-partial').html();
+    Rating.renderRating = Handlebars.compile(Rating.template);
     listRatings();
 });
 
-class Rating {
-    constructor (params) {
-        this.id = params['id'];
-        this.stars = params['stars'];
-        this.comment = params['comment'];
-        this.userName = params['user']['name'];
-    }
+function Rating (attr) {
+    this.id = attr.id;
+    this.stars = attr.stars;
+    this.comment = attr.comment;
+    this.userName = attr.user['name'];
 };
+
+Rating.prototype.renderNewRating = function() {
+    return Rating.renderRating(this)
+}
+
 function listRatings() {
-    $("#js-view-ratings").on("click", function() {
+    $("#js-view-ratings").on("click", function(e) {
         let id = $(this).data("id");
         $.get(`/restrooms/${id}/ratings.json`, function(data) {
-            debugger
-            let ratings = data.map(rating => `${rating['comment']}`).join("<br>");
-            $('#js-ratings').html(ratings);
+            $('#js-ratings').append('<br>')
+            data.forEach(rating => {
+                let rat = new Rating(rating);
+                let ratDiv = rat.renderNewRating()
+                $('#js-ratings').append(ratDiv);
+            });
         });
+        $('a#js-view-ratings').hide();
+        e.preventDefault();
     });
 }
 
@@ -31,6 +41,6 @@ function listRatings() {
 //     });
 // };
 
-function addRating() {
+// function addRating() {
     
-}
+// }
