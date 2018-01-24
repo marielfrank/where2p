019b515a5ae67864ba70a6a19fa02bd0f43ca273
marielfrank <marielbeth@gmail.com>
2018-01-24 12:51:49 -0500

    Add styling to restroom preview div & hide preview button on click in restrooms.js & application.scss

--- app/assets/javascripts/restrooms.js
+++ app/assets/javascripts/restrooms.js
@@ -5,8 +5,9 @@ $(function() {
 function previewRestroom() {
     $(".js-prev").on("click", function() {
         let id = $(this).data("id");
+        $(this).hide();
         $(".preview-rest").hide();
-        $("#preview-rest-" + id).show();
+        $("#preview-rest-" + id).show().addClass('preview-rest-style');
         $.get("/restrooms/" + id + ".json", function(data) {
             $('#preview-address-' + id).html(`<strong>Address:</strong> ${data['address']} in ${data['neighborhood']['name']}`);
             let tags = data['tags'].map(tag => `${tag['description']}`).join("<br>");
